9231b6501c88834c3499f8d3ccf9d36aaf04063a
Jade Masuda <138634124+jymasuda@users.noreply.github.com>
2026-04-04 16:14:53 +0000

    Green: Implementa validação de transferencia com saldo insuficiente

--- src/main/java/contabancaria/Conta.java
+++ src/main/java/contabancaria/Conta.java
@@ -87,6 +87,8 @@ public class Conta {
      *   - O saldo de ambas as contas deve ser atualizado corretamente.
      */
     public void transferir(Conta destino, double valor) {
+        if(this.saldo < valor)
+            throw new IllegalStateException("Saldo insuficiente");
         this.saldo -= valor;
         destino.saldo += valor;
     }
